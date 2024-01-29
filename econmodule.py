451c4907dd622079f5bc7be7a379b62699adf5d9
Nuggets219 <tysonthomas207@gmail.com>
2024-01-29 12:22:18 -0700

    Add main files

---
+++ econmodule.py
@@ -0,0 +1,39 @@
+import io
+econArchive = open("econarchive.txt", "r", encoding="utf-8")
+users = {}
+for i in econArchive:
+    if len(i) > 0:
+        currentUser = i.split("?")
+        users[currentUser[0]] = int(currentUser[1])
+
+def formatNumber(num:int) -> str:
+    return f'{num:,}'
+
+def sayPoints(user):
+    if user.lower() in users:
+        print(f"{user} has {formatNumber(users[user.lower()])} points!")
+
+def setPoints(user : str,points : int):
+    global users
+    users[user.lower()] = points
+
+def changePoints(user : str,diff : int):
+    global users
+    users[user.lower()] += diff
+
+def saveAndQuit():
+    global users,econArchive
+    econArchive.close()
+    econArchive = io.open('econarchive.txt','w')
+    for name in users:
+        econArchive.writelines(f'{name}?{users[name]}\n')
+    econArchive.close()
+while True:
+    exec(input())
+
+"""
+broadcaster?10032
+user1?1234
+user2?1337
+
+"""
