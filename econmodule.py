8e28784f939bd26dd310b50d9819b91937fe91c7
Nuggets219 <tysonthomas207@gmail.com>
2024-01-29 17:49:34 -0700

    update

--- econmodule.py
+++ econmodule.py
@@ -1,36 +1,90 @@
+from config import *
+import time
 import io
-econArchive = open("econarchive.txt", "r", encoding="utf-8") #opening in read mode so its harder to accidentally erase the whole chat's history.
-users = {}
-for i in econArchive:
-    if len(i) > 0:
-        currentUser = i.split("?")
-        users[currentUser[0]] = int(currentUser[1])
+econArchive = io.open("econarchive.txt", "r",encoding="utf-8")  # opening in read mode so its harder to accidentally erase the whole chat's history.
+users = {} #initialize user list
 
-def formatNumber(num:int) -> str:
+def attendance(targUser) -> bool: #users must use the !attendance command to add themselves to the users list, and then they can get points from the routine checks.
+    for i in econArchive:
+        if targUser in i:
+            currentUser = i.split("?")
+            users[currentUser[0]] = int(currentUser[1])
+            return True
+    return False
+
+def pointCountdownGiver(points : int):
+    for person in users:
+        users[person] += points
+
+def pointCountdownInitiator(mins : int):
+    global timerEnd, users
+    users = users.sorted()
+    timerInital = time.time()
+    timerEnd = timerInital + mins * 60
+def checkPointCountdown() -> object:
+    if time.time() >= timerEnd:
+        pointCountdownGiver(pointTimerAmount)
+
+
+def formatNumber(num: int) -> str: #quick formatting for points numbers
     return f'{num:,}'
 
-def sayPoints(user):
-    if user.lower() in users:
-        print(f"{user} has {formatNumber(users[user.lower()])} points!")
+def register(targUser):
+    if targUser not in users:
+        users[targUser] = startingPointAmount
+        return True
+    else:
+        return False
 
-def setPoints(user : str,points : int):
+def transferBal(targUserOld,targUserNew):
+    try:
+        attendance(targUserNew) #make sure both users are loaded into the Users list
+        attendance(targUserOld)
+        users[targUserNew] = users[targUserOld] #transfer balance
+        del users[targUserOld] #remove the entry for the old
+        printToChat('Balance Transfer Successful!')
+    except:
+        printToChat('Balance Transfer Failed!')
+def sayPoints(targUser):
+    if targUser.lower() in users:
+        print(f"{targUser} has {formatNumber(users[targUser.lower()])} points!")
+        printToChat(f"{targUser} has {formatNumber(users[targUser.lower()])} points!")
+    else:
+        return False
+
+def giftPoints(sourceUser,targUser,amount):
+    if 0 < amount <= users[sourceUser]:
+        users[sourceUser] -= amount
+        users[targUser] += amount
+
+def setPoints(targUser: str, points: int):
     global users
-    users[user.lower()] = points
+    users[targUser.lower()] = points
 
-def changePoints(user : str,diff : int):
+
+def changePoints(targUser: str, diff: int):
     global users
-    users[user.lower()] += diff
+    users[targUser.lower()] += diff
 
-def saveAndQuit():
-    global users,econArchive
+
+def saveEconState():
+    global users, econArchive
+
+    #overwrite the backup with the archive state at the beginning of the stream
+    econArchive = io.open("econarchive.txt", "r",encoding="utf-8")
+    econBackup = io.open('econbackup.txt','w',encoding='utf-8')
+    for line in econArchive:
+        if len(line) > 0:
+            econBackup.write(line)
+    econBackup.close()
     econArchive.close()
-    econArchive = io.open('econarchive.txt','w')
+    #overwrite the archive with the current user list
+    econArchive = io.open('econarchive.txt','w',encoding='utf-8')
     for name in users:
         econArchive.writelines(f'{name}?{users[name]}\n')
     econArchive.close()
-    
-while True:
-    exec(input()) #Here for testing purposes
+
+
 
 """
 broadcaster?10032
